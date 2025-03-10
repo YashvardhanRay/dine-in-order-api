4d0638227a3ba96941d6810ff2281d02e7846ea4
Yashvardhan Ray <yashvardhan16200@gmail.com>
2025-03-10 08:56:13 +0530

    adding DTOs for Table and Food Item response

---
+++ src/main/java/com/example/dio/dto/response/FoodItemResponse.java
@@ -0,0 +1,23 @@
+package com.example.dio.dto.response;
+
+import com.example.dio.enums.DietType;
+import com.example.dio.enums.StockStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class FoodItemResponse {
+    private long id;
+    private String name;
+    private double price;
+    private String Description;
+    private StockStatus availability;
+    private DietType dietType;
+    private String cuisineType;
+    private LocalDate createdat;
+    private LocalDateTime lastmodifiedat;
+}
