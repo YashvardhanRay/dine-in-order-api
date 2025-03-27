f0edbde58478ad679f5d85576eb3d8c422bb0f8b
Yashvardhan Ray <yashvardhan16200@gmail.com>
2025-03-27 23:46:46 +0530

    creating findByRestaurantTable function

--- src/main/java/com/example/dio/repository/CartItemRepository.java
+++ src/main/java/com/example/dio/repository/CartItemRepository.java
@@ -1,7 +1,15 @@
 package com.example.dio.repository;
 
 import com.example.dio.model.CartItem;
+import com.example.dio.model.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+
+    @Query("SELECT c FROM CartItem c WHERE c.restaurantTable = :table AND c.ordered = false")
+    List<CartItem> findByRestaurantTable(@Param("table") RestaurantTable table);
 }
