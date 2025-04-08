f0ec96680827c192093a8a5c311361a02606b2ac
Yashvardhan Ray <yashvardhan16200@gmail.com>
2025-04-09 02:15:58 +0530

    made staff registration authenticaticated endpoint by admin and saparated admin endpoint for publicly

--- src/main/java/com/example/dio/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dio/service/impl/UserServiceImpl.java
@@ -1,18 +1,26 @@
 package com.example.dio.service.impl;
 
-import com.example.dio.dto.request.RegistrationRequest;
+import com.example.dio.dto.request.UserRegistrationRequest;
 import com.example.dio.dto.request.UserRequest;
 import com.example.dio.dto.response.UserResponse;
 import com.example.dio.enums.UserRole;
-import com.example.dio.exception.UserNotFoundByIdException;
+import com.example.dio.exception.IllegalRequestException;
+import com.example.dio.exception.RestaurantNotFoundException;
 import com.example.dio.mapper.UserMapper;
 import com.example.dio.model.Admin;
+import com.example.dio.model.Restaurant;
 import com.example.dio.model.Staff;
 import com.example.dio.model.User;
+import com.example.dio.repository.RestaurantRepository;
+import com.example.dio.repository.TableRepository;
 import com.example.dio.repository.UserRepository;
+import com.example.dio.security.util.UserIdentity;
 import com.example.dio.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 
 @Service
@@ -21,11 +29,19 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final TableRepository tableRepository;
+//    private final PasswordEncoder passwordEncoder;
+    private final UserIdentity userIdentity;
+    private final RestaurantRepository restaurantRepository;
 
     @Override
-    public UserResponse registration(RegistrationRequest userRequest) {
-        User user = this.createUserByRole(userRequest.getRole());
-        userMapper.mapToUserRequest(userRequest, user);
+    public UserResponse registration(UserRegistrationRequest registrationRequest) {
+        Admin user = new Admin();
+        userMapper.mapToUserEntity(registrationRequest, user);
+
+//        encryptPassword(user);
+
+        user.setRole(UserRole.ADMIN);
 
        return userMapper.mapToUserResponse( userRepository.save(user));
     }
@@ -54,20 +70,43 @@ public class UserServiceImpl implements UserService {
      */
     @Override
     public UserResponse findUserById(long userId) {
-        User user= userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundByIdException("Failed to find user, user not found by id " + userId));
+        User user = userIdentity.getCurrentUser();
+        userRepository.findById(Long.valueOf(user.getUserid()));
         return userMapper.mapToUserResponse(user);
     }
 
     @Override
-    public UserResponse updateUserById(long userId, UserRequest updatedUser) {
-        return userRepository.findById(userId)
-                .map(exUser -> {
-                    userMapper.mapToUserEntity(updatedUser, exUser);
-                    userRepository.save(exUser);
-                    return userMapper.mapToUserResponse(exUser);
-                })
-                .orElseThrow(() -> new UserNotFoundByIdException("Failed to find user, user not found by id " + userId));
+    public UserResponse updateUserById(UserRequest userRequest) {
+        User user = userIdentity.getCurrentUser();
+        userMapper.mapToUserRequest(userRequest,user);
+        return userMapper.mapToUserResponse(userRepository.save(user));
     }
+
+    @Override
+    public UserResponse staffRegistration(UserRegistrationRequest registrationRequest, long id) {
+
+        Restaurant restaurant = restaurantRepository.findById(id)
+                .orElseThrow(() -> new RestaurantNotFoundException(
+                        "No restaurant found,Enter valid restaurant id"));
+
+        if(restaurant.getCreatedBy().equals(userIdentity.getCurrentUserEmail())){
+            Staff user = new Staff();
+            userMapper.mapToUserEntity(registrationRequest, user);
+//            encryptPassword(user);
+            user.setRole(UserRole.STAFF);
+            user.setRestaurant(restaurant);
+            user.setRestaurantTables(new ArrayList<>(restaurant.getRestaurantTables()));
+            return userMapper.mapToUserResponse(userRepository.save(user));
+        }
+        else{
+            throw new IllegalRequestException(
+                    "Access denied: You are not authorized add staff at "+restaurant.getName());
+        }
+    }
+
+//    private void encryptPassword(User user){
+//        String encodedPassword =  passwordEncoder.encode(user.getPassword());
+//        user.setPassword(encodedPassword);
+//    }
 
 }
